17324911f798a27bc020ca23c6e0e4f4d249562b
Gonzalo Exequiel Pedone <hipersayan.x@gmail.com>
2024-06-25 20:13:58 -0300

    Fix wine install.

--- ports/ci/linux-mingw/install_deps.sh
+++ ports/ci/linux-mingw/install_deps.sh
@@ -74,5 +74,7 @@ ${DOWNLOAD_CMD} "https://sourceforge.net/projects/nsis/files/NSIS%20${NSIS_VERSI
 if [ -e "${nsis}" ]; then
     export WINEPREFIX=/opt/.wine
 
-    xvfb-run --auto-servernum wine ./${nsis} /S
+    wineboot --init
+#     xvfb-run --auto-servernum wine ./${nsis} /S
+    wine ./${nsis} /S
 fi
