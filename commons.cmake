d149e99d1493ed21d7b10e6d91b86dfa25349c4f
Gonzalo Exequiel Pedone <hipersayan.x@gmail.com>
2024-06-28 10:00:16 -0300

    Version up and updated ChangeLog.

--- commons.cmake
+++ commons.cmake
@@ -29,7 +29,7 @@ string(TOLOWER ${COMMONS_APPNAME} COMMONS_TARGET)
 
 set(VER_MAJ 9)
 set(VER_MIN 1)
-set(VER_PAT 1)
+set(VER_PAT 2)
 set(VERSION ${VER_MAJ}.${VER_MIN}.${VER_PAT})
 set(DAILY_BUILD OFF CACHE BOOL "Mark this as a daily build")
 
