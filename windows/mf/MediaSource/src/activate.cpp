e1a786fa0adeda79c7d77218851c277c430409fb
Gonzalo Exequiel Pedone <hipersayan.x@gmail.com>
2025-10-29 09:36:12 -0300

    Try ignoring ShutdownObject().

--- windows/mf/MediaSource/src/activate.cpp
+++ windows/mf/MediaSource/src/activate.cpp
@@ -139,11 +139,5 @@ HRESULT AkVCam::Activate::ShutdownObject()
 {
     AkLogFunction();
 
-    if (this->d->m_mediaSource) {
-        this->d->m_mediaSource->Shutdown();
-        this->d->m_mediaSource->Release();
-        this->d->m_mediaSource = nullptr;
-    }
-
     return S_OK;
 }
